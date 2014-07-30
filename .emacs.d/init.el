c7fffd4bbafc9ad26c3f1cc2ea4c494a622ce91e
masato-nakamura <chariot1897@gmail.com>
2014-07-30 21:19:41 +0900

    init.el追加

---
+++ .emacs.d/init.el
@@ -0,0 +1,149 @@
+;;; ロードパスの追加
+(setq load-path (append
+                 '("~/.emacs.d")
+                 load-path))
+
+;; emacsバージョンの判定
+(defvar oldemacs-p (<= emacs-major-version 22)) ; 22 以下
+(defvar emacs23-p (<= emacs-major-version 23))  ; 23 以下
+(defvar emacs24-p (>= emacs-major-version 24))  ; 24 以上
+(defvar darwin-p (eq system-type 'darwin))      ; Mac OS X 用
+(defvar nt-p (eq system-type 'windows-nt))      ; Windows 用
+
+;; ディレクトリ構成を決める変数
+(when oldemacs-p
+  (defvar user-emacs-directory
+    (expand-file-name (concat (getenv "HOME") "/.emacs.d/"))))
+
+;; 標準Elispの設定
+(load "config/builtins")
+
+;; 非標準Elispの設定
+(load "config/packages")
+
+;;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+
+(setq-default tab-width 4 indent-tabs-mode nil)
+(setq c-default-style "linux")
+(if window-system (progn
+
+(set-face-foreground 'font-lock-comment-face "MediumSeaGreen")
+(set-face-foreground 'font-lock-string-face  "purple")
+(set-face-foreground 'font-lock-keyword-face "blue")
+(set-face-foreground 'font-lock-function-name-face "blue")
+(set-face-bold-p 'font-lock-function-name-face t)
+(set-face-foreground 'font-lock-variable-name-face "black")
+(set-face-foreground 'font-lock-type-face "LightSeaGreen")
+(set-face-foreground 'font-lock-builtin-face "purple")
+(set-face-foreground 'font-lock-constant-face "black")
+(set-face-foreground 'font-lock-warning-face "blue")
+(set-face-bold-p 'font-lock-warning-face nil)
+
+))
+
+;;; 画像ファイルを表示
+(auto-image-file-mode t)
+
+;;; メニューバーを消す
+(menu-bar-mode -1)
+;;; ツールバーを消す
+(tool-bar-mode -1)
+
+;;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+;;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+
+;; 基本
+(define-key global-map (kbd "M-?") 'help-for-help)        ; ヘルプ
+(define-key global-map (kbd "C-z") 'undo)                 ; undo
+(define-key global-map (kbd "C-c i") 'indent-region)      ; インデント
+(define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
+(define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
+(define-key global-map (kbd "M-C-g") 'grep)               ; grep
+(define-key global-map (kbd "C-[ M-C-g") 'goto-line)      ; 指定行へ移動
+
+;; ウィンドウ移動
+;; 次のウィンドウへ移動
+(define-key global-map (kbd "C-M-n") 'next-multiframe-window)
+;; 前のウィンドウへ移動
+(define-key global-map (kbd "C-M-p") 'previous-multiframe-window)
+
+;; 再帰的にgrep
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+(setq grep-command (cons (concat grep-command-before-query " .")
+                         (+ (length grep-command-before-query) 1)))
+
+
+;; 空白や長すぎる行を視覚化する。
+(require 'whitespace)
+;; 1行が80桁を超えたら長すぎると判断する。
+(setq whitespace-line-column 80)
+(setq whitespace-style '(face              ; faceを使って視覚化する。
+                         trailing          ; 行末の空白を対象とする。
+                         lines-tail        ; 長すぎる行のうち
+                                           ; whitespace-line-column以降のみを
+                                           ; 対象とする。
+                         space-before-tab  ; タブの前にあるスペースを対象とする。
+                         space-after-tab)) ; タブの後にあるスペースを対象とする。
+;; デフォルトで視覚化を有効にする。
+(global-whitespace-mode 1)
+
+;;; 現在行を目立たせる
+(global-hl-line-mode)
+
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;;; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+;;; 行の先頭でC-kを一回押すだけで行全体を消去する
+(setq kill-whole-line t)
+
+;;; 最終行に必ず一行挿入する
+(setq require-final-newline t)
+
+;;; バッファの最後でnewlineで新規行を追加するのを禁止する
+(setq next-line-add-newlines nil)
+
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;;; 補完時に大文字小文字を区別しない
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+;;; 補完可能なものを随時表示
+;;; 少しうるさい
+(icomplete-mode 1)
+
+;;; ediffを1ウィンドウで実行
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;;; diffのオプション
+(setq diff-switches '("-u" "-p" "-N"))
+
+;;関数名を表示
+(which-function-mode 1)
